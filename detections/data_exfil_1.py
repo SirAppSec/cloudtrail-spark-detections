9ed12591a1fbfeb7aa09638632900f1b0c1f86e5
Sir AppSec <sirappsec@gmail.com>
2025-03-08 07:28:30 +0200

    detections: data exfil RDB by Exporting to S3 bucket with public read permissions

--- detections/data_exfil_1.py
+++ detections/data_exfil_1.py
@@ -1,9 +1,13 @@
 from pyspark.sql import DataFrame
 from pyspark.sql import functions as F
-
-suspicious_events = ["GetObject"]
+    
+# Export to S3 buckets
 def detect(df: DataFrame) -> DataFrame:
+    suspicious_events = ["StartExportTask"]
     logs = df.filter(
-        F.col("eventName").isin(suspicious_events) 
-    )    
+    F.col("eventName").isin(suspicious_events)# & 
+    # ~F.col("requestParameters.s3BucketName").rlike(
+    #     r"^(backup|audit|internal)-\w+"  # Common benign patterns
+    # )
+)
     return logs
