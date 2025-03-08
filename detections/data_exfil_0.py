9b8df2cbf492325fcc0d86227b728e961e6ff1c9
Sir AppSec <sirappsec@gmail.com>
2025-03-08 06:17:09 +0200

    detections: data_exfil_0.py

--- detections/data_exfil_0.py
+++ detections/data_exfil_0.py
@@ -1,9 +1,9 @@
 from pyspark.sql import DataFrame
 from pyspark.sql import functions as F
 
-suspicious_event = "ModifyDBSnapshotAttribute"
+suspicious_events = ["GetObject"]
 def detect(df: DataFrame) -> DataFrame:
-    return df.filter(
-        (F.col("eventName").isin(suspicious_event)) &
-        (F.col("requestParameters.attributeName") == "restore")
-    )
+    logs = df.filter(
+        F.col("eventName").isin(suspicious_events) 
+    )    
+    return logs
